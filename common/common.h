93743c733ec1fdb378ed91246997578ecdfcb6b9
rong <rongalay@gmail.com>
2015-08-14 12:22:56 -0700

    adding common

---
+++ common/common.h
@@ -0,0 +1,22 @@
+﻿#ifndef _COMMON_
+#define _COMMON_
+
+#include <stdexcept>
+#include <sstream>
+
+using std::stringstream;
+using std::range_error;
+
+typedef unsigned int uint32;
+//#define NULL 0
+
+#define throw_range_error(message) \
+do \
+{ \
+	stringstream ss; \
+	ss << message; \
+	throw range_error(ss.str()); \
+} while (0); \
+
+#endif
+
