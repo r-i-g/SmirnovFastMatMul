82b3b7e60f461b56c120cbd0e2bead66ff995bec
rong <rongalay@gmail.com>
2015-08-14 12:23:58 -0700

    adding matrix

---
+++ matrix/Matrix.cpp
@@ -0,0 +1,117 @@
+#include "Matrix.h"
+#include <stdlib.h>
+#include <sstream>
+
+using SmirnovFastMul::Computation::Matrix;
+using SmirnovFastMul::ArrayType;
+
+
+Matrix::Matrix(double* data, ArrayType array_type, int n, int m, bool is_view):
+m_data(data), m_multiplier(1) ,m_array_type(array_type), m_row_dim(n), m_col_dim(m), m_stride(m), m_is_view(is_view) {}
+
+Matrix::Matrix(int n, int m) : m_data(NULL), m_multiplier(1), m_array_type(), m_row_dim(n), m_col_dim(m), m_stride(m), m_is_view(false) {
+
+	m_array_type.set_array_size(n, m);
+
+	// Allocating n*m double array
+	m_data = new double[n*m];
+}
+
+Matrix::Matrix(int n) : Matrix(n,n) {}
+
+Matrix::Matrix(const Matrix& that) : Matrix(NULL, that.get_mpi_interpretation(), that.get_row_dimension(), that.get_col_dimension(), false) {
+	
+	//// Checking if the dimensions are equal
+	//int that_row_dim = that.get_row_dimension();
+	//int that_col_dim = that.get_col_dimension();
+	//if ((m_row_dim != that_row_dim) ||
+	//	(m_col_dim == that_col_dim)) {
+	//	throw_range_error("Copied matrix differs in dimensions. Expected row_dim " << m_row_dim << " got " << that_row_dim
+	//		 << ". Expected col_dim " << m_col_dim << " got " << that_col_dim);
+	//}
+
+	// Destructing the previous array
+
+
+	// Creating the internal array
+	m_data = new double[m_col_dim*m_row_dim];
+
+	// Copying data
+	double* that_data = that.get_data();
+	for (int i = 0; i < m_row_dim; i++)
+	{
+		for (int j = 0; j < m_col_dim; j++)
+		{
+			m_data[i*m_stride + j] = that_data[i*m_stride + j];
+		}
+	}
+
+	// Setting the multiplier
+	m_multiplier = that.get_multiplier();
+}
+
+ArrayType Matrix::get_mpi_interpretation() const
+{
+	return m_array_type;
+}
+
+double* Matrix::get_data() const
+{
+	return m_data;
+}
+
+double* Matrix::get_data(int i, int j) const
+{
+	return m_data + i*m_stride + j;
+}
+
+void Matrix::update_multiplier(double multiplier)
+{
+	m_multiplier *= multiplier;
+}
+
+void Matrix::set_multiplier(double multiplier)
+{
+	m_multiplier = multiplier;
+}
+
+double Matrix::get_multiplier() const
+{
+	return m_multiplier;
+}
+
+int Matrix::get_row_dimension() const
+{
+	return m_row_dim;
+}
+
+int Matrix::get_col_dimension() const
+{
+	return m_col_dim;
+}
+
+Matrix::~Matrix() {
+	if(!m_is_view) {
+		delete m_data;
+	}
+}
+
+Matrix Matrix::sub_matrix(int num_rows, int num_col, int start_row, int start_col) {
+	m_array_type.set_sub_matrix_size(num_rows, num_col);
+	m_array_type.set_sub_matrix_location(start_row, start_col);
+	// Advancing the data ptr to be at the start of the block
+	return Matrix(get_data(start_row, start_col), m_array_type, num_rows, num_col, true);
+}
+
+/*
+std::ostream& operator<<(std::ostream& os, const Matrix& mat)
+{
+	for (int i = 0; i < mat.get_row_dimension(); ++i) {
+		for (int j = 0; j < mat.get_col_dimension(); ++j) {
+			os << mat(i, j) << " ";
+		}
+		os << std::endl;
+	}
+	return os;
+
+}*/
---
+++ matrix/Matrix.h
@@ -0,0 +1,90 @@
+#ifndef _MATRIX_
+#define _MATRIX_
+
+#include "../common/common.h"
+#include "../communication/MpiCommunication.h"
+#include <mpi.h>
+
+/************************************************************************/
+/*	Creating a MPI matrix wrapper in c++ to ease the handling of sending 
+/*	and receiving of information.
+/*	As a design choice the wrapped mpi calls are in C to preserve backwards
+/*	compatibility.
+/************************************************************************/
+
+namespace SmirnovFastMul {
+	using Communication::ArrayType;
+	namespace Computation
+	{
+		/*
+		Currently the Matrix only supports doubles
+		*/
+		class Matrix {
+		public:
+		
+			Matrix(double* data, const ArrayType matrix_type, int n, int m, bool is_view);
+			Matrix(int n, int m);
+			Matrix(int n);
+	
+			// c-tor - does deep copy
+			Matrix(const Matrix& that);
+	
+			virtual ~Matrix();
+	
+			Matrix sub_matrix(int num_rows, int num_col, int start_row, int start_col);
+			ArrayType get_mpi_interpretation() const;
+	
+			// The indices are 0 based
+			const double& operator()(int i, int j) const { return m_data[i * m_stride + j]; }
+			double& operator()(int i, int j) { return m_data[i * m_stride + j]; }
+			// Returns the entire matrix representation
+			double* get_data() const;
+			double* get_data(int i, int j) const;
+	
+			void update_multiplier(double multiplier);
+			void set_multiplier(double multiplier);
+			
+			double get_multiplier() const;
+			int get_row_dimension() const;
+			int get_col_dimension() const;
+
+			friend std::ostream& operator<<(std::ostream& os, const Matrix& mat) {
+				for (int i = 0; i < mat.get_row_dimension(); ++i) {
+					for (int j = 0; j < mat.get_col_dimension(); ++j) {
+						os << mat(i, j) << " ";
+				}
+				os << std::endl;
+			}
+			return os;
+
+			}
+			// TODO create friend functions
+			// max_ij |a_ij - b_ij|
+			// Frobenius norm difference: \| A - B \|_F
+			// C = alpha1 * A1
+			// C += alpha1 * A1
+			// nullify 
+			// Generate a matrix with random uniform entries on [0, 1]
+			
+			//friend Matrix operator- ();
+	
+		protected:
+			
+			// The data structure decided upon is a continuous block of memory, rows concatenated.
+			double* m_data;
+			double m_multiplier;
+			ArrayType m_array_type;
+	
+			int m_row_dim;
+			int m_col_dim;
+			// Since we save the internal data as a linear vector, we define stride to be m_col_dim-1
+	 		int m_stride;
+
+			// Indicates that this matrix is a view to some other allocated internal data
+			bool m_is_view;
+		};
+	} // Computation namespace
+} // SmirnovFastMul namespace
+
+
+#endif // Matrix class implementation
