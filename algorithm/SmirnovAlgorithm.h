a565a218c3358eb2c297ade6c68f783f8c7e58cf
rong <rongalay@gmail.com>
2015-08-18 12:07:19 -0700

    adding SmirnovAlgorithm

---
+++ algorithm/SmirnovAlgorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by rong on 8/18/15.
+//
+
+#ifndef FASTMATMUL_SMIRNOVALGORITHM_H
+#define FASTMATMUL_SMIRNOVALGORITHM_H
+
+
+class SmirnovAlgorithm {
+
+};
+
+
+#endif //FASTMATMUL_SMIRNOVALGORITHM_H
