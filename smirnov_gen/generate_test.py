0e4ab7ae5aa78640d1d43f845fce97efa63c93bd
rig <rongalay@gmail.com>
2016-05-26 21:49:18 +0300

    refactored to make a test generation framework

--- smirnov_gen/generate_test.py
+++ smirnov_gen/generate_test.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+"""
+A testing generation frame work creating the following products:
+    input file containing the matrices to multiply
+    out file containing the result of the matrices multiplied
+    batch script to run on rhea server or titan server accordingly
+"""
 import sys,random,os
 
 class Matrix:
@@ -23,7 +29,6 @@ class Matrix:
         return Matrix(self.row, self.col, array)
 
     def __mul__(self, other):
-        print other.col, self.row
         array = [[0]*other.col for i in xrange(self.row)]
         for i in xrange(self.row):
             for j in xrange(other.col):
@@ -50,68 +55,138 @@ def matrix_dim(mat):
 def factors(a):
     return [f for f in xrange(1,a+1) if a%f==0]
 
-def create_file(name):
-    folder = os.path.realpath('smirnov_gen/cases/')
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    return open(os.path.join(folder,name), "w")
+class TestGenerator(object):
+
+    def __init__(self, mat1_row, mat1_col, mat2_row, mat2_col, num_sub_problems, proc_row_dim, proc_col_dim):
+        self._proc_row_dim = proc_row_dim
+        self._proc_col_dim = proc_col_dim
+        self._num_sub_problems = num_sub_problems
+        self._nproc = self._proc_row_dim * self._proc_col_dim
+        
+        self.multiply_matrices(mat1_row, mat1_col, mat2_row, mat2_col)
+        self._multiplication_params = "{row1}_{col1}_{col2}_{num_sub_problems}_{proc_row_dim}_{proc_col_dim}".format(row1=self._mat1.row, col1=self._mat1.col, col2=self._mat2.col, 
+                                                                                                                     num_sub_problems=num_sub_problems, 
+                                                                                                                     proc_row_dim=proc_row_dim, proc_col_dim=proc_col_dim)        
+        self._generate_test()
+
+    def create_file(self, name):
+        folder = os.path.realpath('smirnov_gen/cases/')
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        return open(os.path.join(folder,name), "w")
     
+    
+    def multiply_matrices(self, mat1_row, mat1_col, mat2_row, mat2_col):
+        self._mat1 = fill_matrix(mat1_row, mat1_col)
+        self._mat2 = fill_matrix(mat2_row, mat2_col)
+    
+        self._mat1 = Matrix(mat1_row,mat1_col,self._mat1)
+        self._mat2 = Matrix(mat2_row,mat2_col,self._mat2)
+ 
+        self._mat3 = self._mat1 * self._mat2
+        
+    def write_input_matrices(self):
+        input_matrices = self.create_file(self._multiplication_params + ".in")
+        input_matrices.write(matrix_dim(self._mat1))
+        input_matrices.write(matrix_dim(self._mat2))
+        input_matrices.write(str(self._mat1)+'\n')
+        input_matrices.write(str(self._mat2)+'\n')
+        input_matrices.close()
+        
+    def write_multiplication_output(self):
+        multiplication_output = self.create_file(self._multiplication_params+".correct")
+        multiplication_output.write(str(self._mat3)+'\n')
+        multiplication_output.close()
+
+    def write_run_parameters(self):
+        run_params_output = self.create_file(self._multiplication_params+".params")
+        run_params = "mpirun -np {nproc} ./a.out -i cases/{case}.in -c cases/{case}.correct -s {sub_problems} -n {proc_row_dim} -m {proc_col_dim}\n".format(sub_problems=self._num_sub_problems,
+                                                                                                                                               nproc=self._nproc,
+                                                                                                                                               case=self._multiplication_params,
+                                                                                                                                               proc_row_dim=self._proc_row_dim,
+                                                                                                                                               proc_col_dim=self._proc_col_dim)
+        run_params_output.write(run_params)
+        run_params_output.close()
+    
+
+    def write_rhea_job(self):
+        rhea_job_output_file_name = "rhea_{multiplication_params}.pbs".format(multiplication_params=self._multiplication_params)
+        rhea_job_output = self.create_file(rhea_job_output_file_name)
+        job_content = \
+'''#!/bin/bash
+#PBS -A csc182
+#PBS -N {job_name}
+#PBS -o {output_file}
+#PBS -j oe
+#PBS -l walltime=00:05:00,nodes={nproc}
+
+cd $PBS_O_WORKDIR/FastMatMul
+date
+mpirun -np {nproc} ./a.out -i smirnov_gen/cases/{multiplication_params}.in -c smirnov_gen/cases/{multiplication_params}.correct -s {num_sub_problems} -n {proc_row_dim} -m {proc_col_dim}
+'''.format(job_name=self._multiplication_params, 
+           output_file="rhea_{multiplication_params}.txt".format(multiplication_params=self._multiplication_params), 
+           nproc=self._nproc, 
+           multiplication_params=self._multiplication_params, 
+           num_sub_problems=self._num_sub_problems,
+           proc_row_dim=self._proc_row_dim, 
+           proc_col_dim=self._proc_col_dim)
+           
+        rhea_job_output.write(job_content)
+        rhea_job_output.close()
+
+    def write_titan_job(self):
+        titan_job_output_file_name = "titan_{multiplication_params}.pbs".format(multiplication_params=self._multiplication_params)
+        titan_job_output = self.create_file(titan_job_output_file_name)
+        job_content = \
+'''#!/bin/bash
+#PBS -A csc182
+#PBS -N {job_name}
+#PBS -o {output_file}
+#PBS -j oe
+#PBS -l walltime=00:05:00,nodes={nproc}
+#PBS -l gres=atlas1%atlas2
+
+cd $MEMBERWORK/csc182/FastMatMul
+date
+aprun -n {nproc} ./a.out -i smirnov_gen/cases/{multiplication_params}.in -c smirnov_gen/cases/{multiplication_params}.correct -s {num_sub_problems} -n {proc_row_dim} -m {proc_col_dim}
+'''.format(job_name=self._multiplication_params, 
+           output_file="titan_{multiplication_params}.txt".format(multiplication_params=self._multiplication_params), 
+           nproc=self._nproc, 
+           multiplication_params=self._multiplication_params, 
+           num_sub_problems=self._num_sub_problems,
+           proc_row_dim=self._proc_row_dim, 
+           proc_col_dim=self._proc_col_dim)
+           
+        titan_job_output.write(job_content)
+        titan_job_output.close()
+
+    def _generate_test(self):
+        self.write_input_matrices()
+        self.write_multiplication_output()
+        self.write_run_parameters()
+        self.write_rhea_job()
+        self.write_titan_job()
 
 if __name__ == "__main__":
     # Important pramters:
     #   * Matrix dimensions
     #   * Number of supbroblems to calculate in each processor
-    #   * 
+    #   * Dimensions of the processor matrix grid
     
-    if len(sys.argv) < 6:
-        print 'Usage <A row dim> <A col dim> <B col dim> <num sub problems> <num_processes>'
+    if len(sys.argv) < 7:
+        print 'Usage <A row dim> <A col dim> <B col dim> <num sub problems> <processor row dim> <processor col dim>'
         exit(1)
     
+    # Paramter convertion
     mat1_row = int(sys.argv[1])
     mat1_col = int(sys.argv[2])
     mat2_row = mat1_col
     mat2_col = int(sys.argv[3])
     num_sub_problems = int(sys.argv[4])
-    nproc = int(sys.argv[5])
+    proc_row_dim = int(sys.argv[5])
+    proc_col_dim = int(sys.argv[6])
+    nproc = proc_row_dim * proc_col_dim
     
-    oname = "{row1}_{col1}_{col2}_{num_sub_problems}".format(row1=mat1_row, col1=mat1_col, col2=mat2_col, num_sub_problems=num_sub_problems)
+    TestGenerator(mat1_row, mat1_col, mat2_row, mat2_col, num_sub_problems, proc_row_dim, proc_col_dim)
     
-    mat1 = fill_matrix(mat1_row, mat1_col)
-    mat2 = fill_matrix(mat2_row, mat2_col)
-    
-    mat1 = Matrix(mat1_row,mat1_col,mat1)
-    mat2 = Matrix(mat2_row,mat2_col,mat2)
- 
-    mat3 = mat1*mat2
-    
-    o1 = create_file(oname+".in")
-    o1.write(matrix_dim(mat1))
-    o1.write(matrix_dim(mat2))
-    o1.write(str(mat1)+'\n')
-    o1.write(str(mat2)+'\n')
-    o1.close()
-    
-    o2 = create_file(oname+".correct")
-    o2.write(str(mat3)+'\n')
-    o2.close()
-
-    # write valid run parameters
-    proccessor_base = 40 / num_sub_problems
-    alg = {0:3,1:3,2:6}
-    log_grid=0
-    log_alg=0
-    s = min(mat1_row,mat1_col,mat2_col)
-    while s % proccessor_base == 0:
-        s /= proccessor_base
-        log_grid += 1
-    while s % alg[log_alg] == 0:
-        s /= alg[log_alg]
-        log_alg =  (log_alg + 1) % 3
-    
-    o3 = create_file(oname+".params")
-    run_params = "mpirun -np {nproc} ./a.out -i cases/{case}.in -c cases/{case}.correct -s {sub_problems}\n".format(sub_problems=num_sub_problems,
-                                                                                                                        nproc=nproc,
-                                                                                                                        case=oname)
-    o3.write(run_params)
-    o3.close()
                          
--- smirnov_gen/test_cases_generation.py
+++ smirnov_gen/test_cases_generation.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python
 
-from parametes_generator import *
+from generate_params import *
+from generate_test import *
 
-def matrix_ranges(rec_level):
-    #rec_level = base_dim["rec_level"]
-    #init_dim = base_dim['dim']
-    
-    # The amount of sub problems a process takes
-    sub_problems = [1,2,5,8,10,20]
-    for sub_problem in sub_problems:
-        a_dim, b_dim, processor_number, processor_row_dim, processor_col_dim = generate_dimension(rec_level, sub_problem, False)
-        print "For " + str(processor_number) + " processes and " + str(sub_problem) +" amount of subproblems, got matrix sizes of " + str((a_dim, b_dim))
-    
+class TestCasesGenerator(object):
+
+    @staticmethod
+    def _is_valid_setup(matrix_dim_limitation, mat1, mat2):
+        row_limit, col_limit = matrix_dim_limitation
+        return mat1[0] <= row_limit and mat2[0] <= row_limit and mat1[1] <= col_limit and mat2[1] <= col_limit
+        
+    @staticmethod
+    def generate_test_set(matrix_dim_limitation):
+        row_limitation, col_limitation = matrix_dim_limitation
+        sub_problems = [1,2,5,8,10,20]
+        for rec_level in range(2,5):
+            for sub_problem in sub_problems:
+                mat1, mat2, processor_number, processor_row_dim, processor_col_dim = generate_dimension(rec_level, sub_problem, False)
+                if TestCasesGenerator._is_valid_setup(matrix_dim_limitation, mat1, mat2):
+                    print "For " + str(processor_number) + " processes and " + str(sub_problem) +" amount of subproblems, got matrix sizes of " + str((mat1, mat2))
+                    TestGenerator(mat1[0], mat1[1], mat2[0], mat2[1], sub_problem, processor_row_dim, processor_col_dim)
+   
     
 if __name__ == "__main__":
     
-    # Different levels of recursion (3,3,6), (9,18,18), (54,54,54)
-    base_matrix_dim = [(9,18,18)]
-    
-    #for rec_level in range(5):
-    for rec_level in range(2,5):
-        print "For " + str(rec_level) + " recursion levels"
-        matrix_ranges(rec_level)
-        print 
+    TestCasesGenerator.generate_test_set((5000,5000))
         
